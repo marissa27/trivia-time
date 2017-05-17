b293a0bb788cca0ad307483604ae7fad9f78c6df
Marissa Reinke <reinke.marissa@gmail.com>
2017-05-16 19:45:51 -0600

    Setup the server.js

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Trivia Time!';
+app.locals.title = 'Trivia Time';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 app.get('/', (request, response) => {
-  response.send('It is trivia up in here')
+  fs.readFile(`${__dirname}/index.html`, (err, file) => {
+    response.send(file)
+  })
 });
 
-app.listen(app.get('port'), () => {
-  console.log('Your app is running')
-});
+if (!module.parent) {
+  app.listen(app.get('port'), () => {
+    console.log('Your app is running!')
+  })
+}
+
+module.exports = app;
