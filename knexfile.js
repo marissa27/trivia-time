e7346a5be15e795c2b9fce5f6f34970cca60892f
Marissa Reinke <reinke.marissa@gmail.com>
2017-05-16 19:29:00 -0600

    Add public folder and cleanup knex

--- knexfile.js
+++ knexfile.js
@@ -9,38 +9,5 @@ module.exports = {
       directory: './db/migrations'
     },
     useNullAsDefault: true
-  },
-
-  staging: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'pg',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
   }
-
 };
